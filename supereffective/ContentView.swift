0382e9320d637b9576d4db7f94d1dd919e97fb08
sweetclimusic <4057640+sweetclimusic@users.noreply.github.com>
2024-04-30 21:50:46 +0100

    Initial Commit

---
+++ supereffective/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  supereffective
+//
+//  Created by Ashlee Muscroft on 30/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ supereffective/supereffectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  supereffectiveApp.swift
+//  supereffective
+//
+//  Created by Ashlee Muscroft on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct supereffectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
